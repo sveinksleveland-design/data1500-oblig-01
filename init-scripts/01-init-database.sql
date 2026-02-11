865dd8739f87872d34e9fba49d03ed10306df3de
sveinksleveland-design <sveink.sleveland@gmail.com>
2026-02-11 15:42:11 +0100

    Update 01-init-database.sql

--- init-scripts/01-init-database.sql
+++ init-scripts/01-init-database.sql
@@ -4,15 +4,85 @@
 -- ============================================================================
 
 -- Opprett grunnleggende tabeller
+CREATE TABLE kunde (
+    kunde_id VARCHAR(100) PRIMARY KEY,
+    mobilnummer VARCHAR(8),
+    epost VARCHAR(254),
+    fornavn VARCHAR(20),
+    etternavn VARCHAR(15)
+);
 
+
+CREATE TABLE sykkelstasjoner (
+    stasjon_id VARCHAR(10) PRIMARY KEY,
+    parkering_plass INTEGER
+);
+
+
+CREATE TABLE lås (
+    las_id INTEGER PRIMARY KEY,
+    stasjon_id VARCHAR(10),
+    FOREIGN KEY (stasjon_id)
+        REFERENCES sykkelstasjoner(stasjon_id)
+);
+
+
+CREATE TABLE sykler (
+    sykkel_id VARCHAR(20) PRIMARY KEY,
+    stasjon_id VARCHAR(10),
+    FOREIGN KEY (stasjon_id)
+        REFERENCES sykkelstasjoner(stasjon_id)
+);
+
+
+CREATE TABLE utleie (
+    utleie_id INTEGER PRIMARY KEY,
+    kunde_id VARCHAR(100),
+    sykkel_id VARCHAR(20),
+    start_tid TIMESTAMP,
+    slutt_tid TIMESTAMP,
+    FOREIGN KEY (kunde_id) REFERENCES kunde(kunde_id),
+    FOREIGN KEY (sykkel_id) REFERENCES sykler(sykkel_id)
+);
 
 
 -- Sett inn testdata
+-- kunde table:
+INSERT INTO kunde VALUES
+('k001','12345678','ola@test.no','Ola','Nordmann'),
+('k002','87654321','kari@test.no','Kari','Hansen'),
+('k003','11223344','Per@test.no','Per','Johansen'),
+('k004','99887766','Anne@test.no','Anne','Berg'),
+('k005','55667788','Lars@test.no','Lars','Olsen');
 
+-- sykkelstasjoner:
+INSERT INTO sykkelstasjoner VALUES
+('S01',20),
+('S02',20),
+('S03',20),
+('S04',20),
+('S05',20);
 
+-- 100 låser, 20 per stasjon
+INSERT INTO lås
+SELECT i,
+       CONCAT('S0', ((i-1)/20)+1)
+FROM generate_series(1,100) AS i;
+
+-- 100 sykler
+INSERT INTO sykler
+SELECT CONCAT('B', LPAD(i::text,3,'0')),
+       CONCAT('S0', ((i-1)/20)+1)
+FROM generate_series(1,100) AS i;
 
 -- DBA setninger (rolle: kunde, bruker: kunde_1)
+CREATE ROLE kunde;
+GRANT SELECT ON sykler TO kunde;
+GRANT SELECT ON sykkelstasjoner TO kunde;
+GRANT SELECT, INSERT ON utleie TO kunde;
 
+CREATE USER kunde_1 WITH PASSWORD 'passord123';
+GRANT kunde TO kunde_1;
 
 
 -- Eventuelt: Opprett indekser for ytelse
